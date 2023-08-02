85d3f49788f350df1d05fd2d6a27d6ecbbdd6b5d
joeinman <joe.inman8@gmail.com>
2023-08-02 11:22:04 +0100

    Implemented interactive console

--- src/FlashKVDev.cpp
+++ src/FlashKVDev.cpp
@@ -3,8 +3,21 @@
 #include <optional>
 #include <iomanip>
 #include <vector>
+#include <sstream>
 
 #include <FlashKV/FlashKV.h>
+
+void printHelp()
+{
+    std::cout << "Available Commands:\n"
+              << "read <key> - Reads a value associated with a key from the store.\n"
+              << "readall - Reads all keys in the store.\n"
+              << "write <key> <value> - Writes a key-value pair to the store. Value should be a sequence of bytes in hexadecimal format.\n"
+              << "erase <key> - Erases a key-value pair from the store.\n"
+              << "eraseall - Erases all key-value pairs from the store.\n"
+              << "save - Saves the key-value store to Flash memory.\n"
+              << "exit - Exits the program.\n";
+}
 
 int main(int argc, char *argv[])
 {
@@ -69,72 +82,141 @@ int main(int argc, char *argv[])
     // Load The Store From File
     int loadStatus = flashKV.loadStore();
     if (loadStatus == 0)
-        std::cout << "FlashKV Store Loaded Successfully" << std::endl;
+        std::cout << "[INFO] FlashKV Store Loaded Successfully" << std::endl;
     else if (loadStatus == 1)
-        std::cout << "No FlashKV Store Found. A New One Will Be Created Upon Saving" << std::endl;
-    else
-        std::cout << "Error Occurred While Loading The FlashKV Store" << std::endl;
-
-    // Try To Read A Value From The Store
-    std::string key = "test";
-    std::optional<std::vector<uint8_t>> value = flashKV.readKey(key);
-    if (value)
-    {
-        std::cout << "Read Key: [" << key << "], Value: [";
-        for (auto it = value->begin(); it != value->end(); ++it)
-        {
-            if (it != value->begin())
-                std::cout << " ";
-            std::cout << std::hex << std::setw(2) << std::setfill('0') << (int)*it;
-        }
-        std::cout << "]" << std::endl;
-    }
+        std::cout << "[INFO] No FlashKV Store Found. A New One Will Be Created Upon Saving" << std::endl;
     else
     {
-        std::cout << "Key [" << key << "] Not Found." << std::endl;
+        std::cout << "[ERROR] Error Occurred While Loading The FlashKV Store" << std::endl;
+        return 1;
     }
 
-    // Write A Value To The Store
-    std::vector<uint8_t> data = {0x01, 0x02, 0x03, 0x04};
-    flashKV.writeKey(key, data);
-
-    // Write Another Value To The Store
-    flashKV.writeKey("test2", {0x05, 0x06, 0x07, 0x08});
-
-    std::cout << "Writing Key: [" << key << "], Value: [";
-    for (auto it = data.begin(); it != data.end(); ++it)
+    std::string command;
+    while (1)
     {
-        if (it != data.begin())
-            std::cout << " ";
-        std::cout << std::hex << std::setw(2) << std::setfill('0') << (int)*it;
-    }
-    std::cout << "]" << std::endl;
+        std::cout << "> ";
+        std::getline(std::cin, command);
 
-    std::cout << "All Keys:" << std::endl;
-    auto allKeys = flashKV.getAllKeys();
-    for (auto key : allKeys)
-    {
-        std::cout << "Key: [" << key << "], Value: [";
-        std::optional<std::vector<uint8_t>> value = flashKV.readKey(key);
-        if (value)
+        std::istringstream iss(command);
+        std::string op;
+        iss >> op;
+
+        if (op == "read")
         {
-            for (auto it = value->begin(); it != value->end(); ++it)
+            std::string key;
+            while (iss >> key)
             {
-                if (it != value->begin())
-                    std::cout << " ";
-                std::cout << std::hex << std::setw(2) << std::setfill('0') << (int)*it;
+                std::optional<std::vector<uint8_t>> value = flashKV.readKey(key);
+                if (value)
+                {
+                    std::cout << "[INFO] Read Key: \"" << key << "\", Value: [";
+                    for (auto it = value->begin(); it != value->end(); ++it)
+                    {
+                        if (it != value->begin())
+                            std::cout << " ";
+                        std::cout << std::hex << std::setw(2) << std::setfill('0') << (int)*it;
+                    }
+                    std::cout << "]" << std::endl;
+                }
+                else
+                {
+                    std::cout << "[ERROR] Key \"" << key << "\" Not Found" << std::endl;
+                }
             }
+        }
+        else if (op == "readall")
+        {
+            auto allKeys = flashKV.getAllKeys();
+            std::cout << "[INFO] Found " << allKeys.size() << (allKeys.size() == 1 ? " Key" : " Keys") << std::endl;
+
+            for (auto key : allKeys)
+            {
+                std::cout << "[INFO] Key: \"" << key << "\", Value: [";
+                std::optional<std::vector<uint8_t>> value = flashKV.readKey(key);
+                for (auto it = value->begin(); it != value->end(); ++it)
+                {
+                    if (it != value->begin())
+                        std::cout << " ";
+                    std::cout << std::hex << std::setw(2) << std::setfill('0') << (int)*it;
+                }
+                std::cout << "]" << std::endl;
+            }
+        }
+        else if (op == "write")
+        {
+            std::string key;
+            if (!(iss >> key))
+            {
+                std::cout << "[Error] No key provided.\n";
+                continue;
+            }
+
+            std::vector<uint8_t> value;
+            std::string byteStr;
+            while (iss >> byteStr)
+            {
+                if (byteStr.size() != 2)
+                {
+                    std::cout << "[ERROR] Invalid Byte: " << byteStr << std::endl;
+                    break;
+                }
+                unsigned int byte = std::stoul(byteStr, nullptr, 16);
+                value.push_back(static_cast<uint8_t>(byte));
+            }
+
+            if (flashKV.writeKey(key, value))
+                std::cout << "[INFO] Key \"" << key << "\" Written Successfully" << std::endl;
+            else
+                std::cout << "[ERROR] Unable To Write Key \"" << key << "\"" << std::endl;
+        }
+        else if (op == "erase")
+        {
+            std::string key;
+            while (iss >> key)
+            {
+                if (flashKV.eraseKey(key))
+                    std::cout << "[INFO] Key \"" << key << "\" Erased Successfully" << std::endl;
+                else
+                    std::cout << "[Error] Key \"" << key << "\" Doesn't Exist" << std::endl;
+            }
+        }
+        else if (op == "eraseall")
+        {
+            auto allKeys = flashKV.getAllKeys();
+            for (auto key : allKeys)
+            {
+                if (flashKV.eraseKey(key))
+                    std::cout << "[INFO] Key \"" << key << "\" Erased Successfully" << std::endl;
+                else
+                    std::cout << "[Error] Key \"" << key << "\" Doesn't Exist" << std::endl;
+            }
+        }
+        else if (op == "save")
+        {
+            // Save The Store To Flash
+            if (flashKV.saveStore())
+                std::cout << "[INFO] FlashKV Store Saved Successfully" << std::endl;
+            else
+                std::cout << "[ERROR] Error Occurred While Saving The FlashKV Store" << std::endl;
+        }
+        else if (op == "help")
+        {
+            printHelp();
+        }
+        else if (op == "exit")
+        {
+            // Save The Store To Flash
+            if (flashKV.saveStore())
+                std::cout << "[INFO] FlashKV Store Saved Successfully" << std::endl;
+            else
+                std::cout << "[ERROR] Error Occurred While Saving The FlashKV Store" << std::endl;
+            break;
         }
         else
         {
-            std::cout << "Not Found";
+            std::cout << "[INFO] Unknown command: \"" << op << "\". Type 'help' For A List 0f Commands.\n";
         }
-        std::cout << "]" << std::endl;
     }
 
-    // Save The Store To Flash
-    if (flashKV.saveStore())
-        std::cout << "FlashKV Store Saved Successfully" << std::endl;
-    else
-        std::cout << "Error Occurred While Saving The FlashKV Store" << std::endl;
+    return 0;
 }
